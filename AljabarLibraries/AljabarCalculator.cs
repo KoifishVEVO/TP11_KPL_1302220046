690f9241995937a727f5b0c47ae51569fbe7a9a7
KoifishVEVO <farhan210604@gmail.com>
2024-05-05 19:17:51 +0700

    menambahkan console dan mengimplementasi library

--- AljabarLibraries/AljabarCalculator.cs
+++ AljabarLibraries/AljabarCalculator.cs
@@ -1,7 +1,44 @@
-﻿namespace AljabarLibraries
+﻿using System.ComponentModel.Design.Serialization;
+
+namespace AljabarLibraries
 {
     public class AljabarCalculator
     {
+
+        public double[] AkarPersamaanKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+            double c = persamaan[2];
+
+            double[] roots;
+
+            double discriminant = b * b - 4 * a * c;
+
+            if (discriminant > 0)
+            {
+                double root1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+                double root2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+
+                roots = new double[] {root1, root2};
+            } else if (discriminant == 0)
+            {
+                double root = -b / (2 * a);
+                roots = new double[] { root, root };
+            } else
+            {
+                roots = new double[0];
+            }
+            return roots;
+        }
+        public double[] HasilKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+
+            return new double[] { (a * a) + (2 * a * b) + (b * b) };
+        }
+
 
     }
 }
---
+++ consoler/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AljabarLibraries;
+using System;
+
+class Program
+{
+        static void Main(string[] args)
+        {
+            
+            AljabarCalculator calculator = new AljabarCalculator();
+
+        
+            
+            double[] akar = calculator.AkarPersamaanKuadrat([ 1, -3, -10 ]);
+            Console.WriteLine("Akar persamaan kuadrat 1: " + akar[0] + ", "+ akar[1]);
+
+          
+            
+            double[] hasilKuadrat = calculator.HasilKuadrat([2,-3]);
+            Console.WriteLine("Hasil kuadrat persamaan 2: " + hasilKuadrat[0]);
+        }
+    
+}
+
